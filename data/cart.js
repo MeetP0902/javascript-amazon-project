8bf85fef8fd6ce016037f872dac71975f0c3e167
MeetP <meetpatel7581@gmail.com>
2025-08-15 23:28:56 -0400

    save data and generate html for delivery

--- data/cart.js
+++ data/cart.js
@@ -1,9 +1,11 @@
 export let cart = JSON.parse(localStorage.getItem('cart'))||[{
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity:4
+  quantity:4,
+  deliveryOptionID:'1'
 },{
   id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
+  quantity: 1,
+  deliveryOptionID:'2'
 }]
 
 function saveToStorage(){
@@ -21,7 +23,8 @@ export function addToCart(productID){
   }else{
     cart.push({
       id:productID,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionID:'1'
    })
   }
   saveToStorage()

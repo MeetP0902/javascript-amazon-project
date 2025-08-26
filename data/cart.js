33e327e37025b0ccf08eb486444b5174399a4946
MeetP <meetpatel7581@gmail.com>
2025-08-26 19:39:31 -0400

    learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -57,3 +57,15 @@ export function updateDeliveryOption(productID,deliveryOptionID){
   matching.deliveryOptionID = deliveryOptionID
   saveToStorage()
 }
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest
+  xhr.addEventListener('load',()=>{
+    
+    
+    console.log(xhr.response)
+    fun();
+  })
+  xhr.open('GET', 'https://supersimplebackend.dev/cart' )
+  xhr.send()
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,34 @@
 import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js '
 
-loadProducts(()=>{
+
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('value1')
+    })
+  }),new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve()
+    })
+  })
+]).then(()=>{
   renderOrderSummary();
-  renderPaymentSummary();
+  renderPaymentSummary()
 })
+
+
+// loadProducts(()=>{
+//   loadCart(()=>{
+//    renderOrderSummary();
+//    renderPaymentSummary();
+//   })
+// })
+
+
 
 
